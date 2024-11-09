26a7b793fc2c70d6e23d2d9efe68c5b7bd66f80e
КириллДоминов <spotik321@gmail.com>
2024-11-09 12:47:19 +0500

    Done

---
+++ src/main/java/pro/sky/skyprocalculator/SkyproCalculatorApplication.java
@@ -0,0 +1,12 @@
+package pro.sky.skyprocalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproCalculatorApplication.class, args);
+	}
+}
